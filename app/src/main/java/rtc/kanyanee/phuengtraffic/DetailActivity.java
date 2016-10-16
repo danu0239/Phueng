78eb3cead530bc6b57c61838343606d09e85e1c2
ACER <phueng2904.nu@gmail.com>
2016-10-16 14:21:45 +0700

    design UI detail a

--- app/src/main/java/rtc/kanyanee/phuengtraffic/DetailActivity.java
+++ app/src/main/java/rtc/kanyanee/phuengtraffic/DetailActivity.java
@@ -2,12 +2,29 @@ package rtc.kanyanee.phuengtraffic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+
+    // Explicit
+    private TextView titleTextView, detailTextView;
+    private ImageView imageView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-    }
-}
+
+        // Bind Widget
+        titleTextView = (TextView) findViewById(R.id.textView4);
+        detailTextView = (TextView) findViewById(R.id.textView5);
+        imageView = (ImageView) findViewById(R.id.imageView2);
+        button = (Button) findViewById(R.id.button2);
+
+
+    }   // Main Method
+
+}   // Main Class
--- app/src/main/java/rtc/kanyanee/phuengtraffic/MainActivity.java
+++ app/src/main/java/rtc/kanyanee/phuengtraffic/MainActivity.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -49,16 +50,27 @@ public class MainActivity extends AppCompatActivity {
         // ListView Controller
         // Get Value from Class
         MyConstant myConstant = new MyConstant();
-        int[] ints = myConstant.getInts();
+        final int[] ints = myConstant.getInts();
 
         // Get Value From xml
-        String[] titleStrings = getResources().getStringArray(R.array.title);
-        String[] detailStrings = getResources().getStringArray(R.array.detail);
+        final String[] titleStrings = getResources().getStringArray(R.array.title);
+        final String[] detailStrings = getResources().getStringArray(R.array.detail);
 
         //Create ListView
         TrafficAdapter trafficAdapter = new TrafficAdapter(MainActivity.this,
                 titleStrings, detailStrings, ints);
         listView.setAdapter(trafficAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                intent.putExtra("Title", titleStrings[position]);
+                intent.putExtra("Image", ints[position]);
+                intent.putExtra("Detail", detailStrings[position]);
+                startActivity(intent);
+            }
+        });
 
     }   // Main Method กลุ่มของคำสั่ง
 
