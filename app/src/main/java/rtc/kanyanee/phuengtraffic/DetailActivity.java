62f76a300b6f415a41c700cc2ace0e8aa70e3580
ACER <phueng2904.nu@gmail.com>
2016-10-16 14:32:48 +0700

    Black Ok

--- app/src/main/java/rtc/kanyanee/phuengtraffic/DetailActivity.java
+++ app/src/main/java/rtc/kanyanee/phuengtraffic/DetailActivity.java
@@ -2,6 +2,7 @@ package rtc.kanyanee.phuengtraffic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -23,6 +24,20 @@ public class DetailActivity extends AppCompatActivity {
         detailTextView = (TextView) findViewById(R.id.textView5);
         imageView = (ImageView) findViewById(R.id.imageView2);
         button = (Button) findViewById(R.id.button2);
+
+        // Show View
+        titleTextView.setText(getIntent().getStringExtra("Title"));
+        detailTextView.setText(getIntent().getStringExtra("Detail"));
+        imageView.setImageResource(getIntent().getIntExtra("Image", R.drawable.traffic_01));
+
+        // Button Controller
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
 
 
     }   // Main Method
